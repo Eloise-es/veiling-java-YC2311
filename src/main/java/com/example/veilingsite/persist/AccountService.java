41f75f90026c73ac8bb51b687c97c16ebbd20dfc
Eloise-es <101559841+Eloise-es@users.noreply.github.com>
2023-11-24 11:44:46 +0100

    Cors config

--- src/main/java/com/example/veilingsite/persist/AccountService.java
+++ src/main/java/com/example/veilingsite/persist/AccountService.java
@@ -4,15 +4,20 @@ import com.example.veilingsite.domain.Account;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
 public class AccountService {
     @Autowired
     AccountRepository ar;
 
-    public Account createAccount() {
-        Account a = new Account();
-        a.setNaam("Eloise");
+    public Account createAccount(Account a) {
         ar.save(a);
         return a;
+    }
+
+    public Account getAccount(long id) {
+        Optional<Account> a = ar.findById(id);
+        return a.orElse(null);
     }
 }
--- src/main/java/com/example/veilingsite/view/AccountEndpoint.java
+++ src/main/java/com/example/veilingsite/view/AccountEndpoint.java
@@ -3,19 +3,31 @@ package com.example.veilingsite.view;
 import com.example.veilingsite.domain.Account;
 import com.example.veilingsite.persist.AccountService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.sql.SQLOutput;
 
 @RestController
 public class AccountEndpoint {
     @Autowired
     AccountService as;
 
-    @GetMapping("/maak-account/{naam}")
-    public Account maakAccount(@PathVariable("naam") String naam) {
+    @GetMapping("/account/{id}")
+    public Account zoekAccount(@PathVariable("id") long id) {
         System.out.println("Hij doet het");
-        Account a = as.createAccount();
-        return a;
+        return as.getAccount(id);
+    }
+
+    @GetMapping("/alle-accounts")
+    public String getAccounts() {
+        System.out.println("Hij doet het");
+        return "hi";
+    }
+
+    @PostMapping("maak-account")
+    public Account maakAccount(@RequestBody Account a) {
+        System.out.println(a.getNaam());
+        return as.createAccount(a);
+
     }
 }
