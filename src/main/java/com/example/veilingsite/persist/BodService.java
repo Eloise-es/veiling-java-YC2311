88e01bb15b9d65679459c9b9a20dfb42f86e4bc5
Eloise-es <101559841+Eloise-es@users.noreply.github.com>
2023-12-19 13:24:23 +0100

    Bod repository, service + endpoint

---
+++ src/main/java/com/example/veilingsite/persist/BodService.java
@@ -0,0 +1,12 @@
+package com.example.veilingsite.persist;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class BodService {
+    @Autowired
+    BodRepository br;
+
+    // CREATE
+}
---
+++ src/main/java/com/example/veilingsite/view/BodEndpoint.java
@@ -0,0 +1,5 @@
+package com.example.veilingsite.view;
+
+public class BodEndpoint {
+    
+}
